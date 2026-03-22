1788ce7f124f7b0c9ce2c1ab171a267bf31e0cab
popnegro <grasso.luis@gmail.com>
2026-03-22 01:49:57 -0300

    Reorder file

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 const CACHE_NAME = 'taxigo-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './theme.min.css',
-  './taxigo-icon.webp',
-  './slide-02.webp'
+const assets = [
+  'index.html',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
